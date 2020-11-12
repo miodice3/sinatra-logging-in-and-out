82e0dc68e29ff7c258ac181392a80669707f0183
Michael Iodice <miodice3@gmail.com>
2020-11-12 15:04:34 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,32 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if @user
+      session[:user_id] = @user.id
+      
+      redirect to '/account'
+    elsif @user == nil
+      
+      erb :error
+    end
 
   end
 
   get '/account' do
-
+    @user = User.find_by_id(session[:user_id])
+    if @user
+      erb :account
+    elsif @user == nil
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
 
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
+require 'pry'
+class User < ActiveRecord::Base
 
+    # @@all = []
+
+    # attr_accessor :username, :password, :balance
+
+    # def initialize(hash)
+    #     # binding.pry
+    #     @username = hash[:username]
+    #     @password = hash[:password]
+    #     @balance = hash[:balance]
+    #     @@all << self
+    #     binding.pry
+    # end
+
+    # def self.all
+    #     @@all
+    # end
+
+end
 
